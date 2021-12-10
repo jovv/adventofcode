854d55ad551848031cb8a6c147b49b0d47df2b74
Jochen Van de Voorde <jochen.vandevoorde@gmail.com>
2021-12-10 23:10:35 +0100

    Add 2021 day 10 puzzle part 2

--- 2021/days/day_10.py
+++ 2021/days/day_10.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import re
 from typing import List
 
@@ -10,30 +11,47 @@ illegal_character_scores = {
     '>': 25137,
 }
 
+completed_character_scores = {
+    '(': 1,
+    '[': 2,
+    '{': 3,
+    '<': 4,
+}
+
 matching_re = re.compile(r'\(\)|\[\]|\{\}|\<\>')
 illegal_re = re.compile(r'\)|\]|\}|\>')
 
 
-def syntax_error_score(lines: List[str]) -> int:
-    score = 0
+def scores(lines: List[str]) -> int:
+    illegal_score = 0
+    autocomplete_scores = []
 
     for line in lines:
         nr_of_subs = 1
         while nr_of_subs > 0:
             line, nr_of_subs = matching_re.subn('', line)
         m = illegal_re.search(line)
-        if m:
-            score += illegal_character_scores[m.group(0)]
+        if m:  # corrupt line
+            illegal_score += illegal_character_scores[m.group(0)]
+        else:  # incomplete line
+            autocomplete_scores.append(
+                reduce(
+                    lambda a, b: 5 * a + b,
+                    [completed_character_scores[c] for c in line[::-1]],
+                    0,
+                ))
 
-    return score
+    autocomplete_middle_score = sorted(autocomplete_scores)[
+        len(autocomplete_scores) // 2]
+
+    return illegal_score, autocomplete_middle_score
 
 
 if __name__ == '__main__':
 
     content = day_input(f'{day(__file__)}_input.txt')
-
-    part1 = syntax_error_score(content)
-    # part2 = syntax_error_score(content)
+    lines = [line.strip() for line in content]
+    part1, part2 = scores(lines)
 
     print(part1)
-    # print(part2)
+    print(part2)
--- 2021/tests/day_10_test.py
+++ 2021/tests/day_10_test.py
@@ -1,4 +1,4 @@
-from days.day_10 import syntax_error_score
+from days.day_10 import scores
 
 sample_content = [
     '[({(<(())[]>[[{[]{<()<>>',
@@ -15,4 +15,10 @@ sample_content = [
 
 
 def test_syntax_error_score():
-    assert syntax_error_score(sample_content) == 26397
+    illegal, _ = scores(sample_content)
+    assert illegal == 26397
+
+
+def test_autocomplete_middle_score():
+    _, middle = scores(sample_content)
+    assert middle == 288957
