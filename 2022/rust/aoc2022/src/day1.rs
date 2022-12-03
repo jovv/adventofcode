7f0b2193ccff35014105b657713464e36eeaffeb
Jochen Van de Voorde <jochen.van.de.voorde@persgroep.net>
2022-12-03 22:11:39 +0100

    Refactor Rust solution structure

--- 2022/rust/aoc2022/src/day1.rs
+++ 2022/rust/aoc2022/src/day1.rs
@@ -1,22 +1,49 @@
-pub fn solution(s: String) -> (String, String) {
-    (part1(s.clone()), part2(s))
+use std::fmt;
+// use crate::day::Day;
+
+pub struct Day {
+    part1: String,
+    part2: String,
 }
 
-fn part1(s: String) -> String {
-    format!("{}", cargo_per_elf(s).iter().max().unwrap())
+impl fmt::Display for Day {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}\n{}", self.part1, self.part2)
+    }
 }
 
-fn part2(s: String) -> String {
-    let mut cpe = cargo_per_elf(s);
-    cpe.sort();
-    cpe.reverse();
-    format!("{}", cpe[0..3].iter().sum::<u32>())
+pub trait Solution {
+    fn new(s: String) -> Self;
+    fn part1(s: String) -> String;
+    fn part2(s: String) -> String;
 }
 
-fn cargo_per_elf(s: String) -> Vec<u32> {
-    s.clone()
-        .split("\n\n")
-        .map(|s| s.split_whitespace().map(|s| s.parse::<u32>().unwrap()))
-        .map(|xs| xs.sum())
-        .collect()
+impl Day {
+    fn cargo_per_elf(s: String) -> Vec<u32> {
+        s.clone()
+            .split("\n\n")
+            .map(|s| s.split_whitespace().map(|s| s.parse::<u32>().unwrap()))
+            .map(|xs| xs.sum())
+            .collect()
+    }
+}
+
+impl Solution for Day {
+    fn new(s: String) -> Day {
+        Day {
+            part1: Self::part1(s.clone()),
+            part2: Self::part2(s),
+        }
+    }
+
+    fn part1(s: String) -> String {
+        format!("{}", Self::cargo_per_elf(s).iter().max().unwrap())
+    }
+
+    fn part2(s: String) -> String {
+        let mut cpe = Self::cargo_per_elf(s);
+        cpe.sort();
+        cpe.reverse();
+        format!("{}", cpe[0..3].iter().sum::<u32>())
+    }
 }
--- 2022/rust/aoc2022/src/main.rs
+++ 2022/rust/aoc2022/src/main.rs
@@ -1,22 +1,21 @@
 use std::fs;
 mod day1;
 
+use day1::Solution;
+
 fn main() {
     let day: u8 = 1;
     let puzzle_input_filename = format!("./resources/day{day}.txt");
 
     match fs::read_to_string(&puzzle_input_filename) {
-        Ok(s) => {
-            let (part1, part2) = solution(day, s);
-            println!("{part1}\n{part2}");
-        }
+        Ok(s) => println!("{}", solution(day, s)),
         Err(e) => panic!("Failed to get puzzle input for {puzzle_input_filename}, msg: {e}"),
     }
 }
 
-fn solution(day: u8, s: String) -> (String, String) {
+fn solution(day: u8, s: String) -> day1::Day {
     match day {
-        1 => day1::solution(s),
+        1 => day1::Day::new(s),
         _ => panic!("No solution branch for day {day}"),
     }
 }
