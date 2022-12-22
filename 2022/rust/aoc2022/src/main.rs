159b07f595c11976cb29e2ed6fb34ad68423b191
Jochen Van de Voorde <jochen.van.de.voorde@persgroep.net>
2022-12-22 11:52:18 +0100

    Update Rust main

--- 2022/rust/aoc2022/src/main.rs
+++ 2022/rust/aoc2022/src/main.rs
@@ -3,11 +3,14 @@ mod day;
 mod day1;
 mod day12;
 mod day15;
+mod day2;
+mod day3;
+mod day4;
 
 use day::Solution;
 
 fn main() {
-    let day: u8 = 15;
+    let day: u8 = 4;
     let puzzle_input_filename = format!("./resources/day{day}.txt");
 
     match fs::read_to_string(&puzzle_input_filename) {
@@ -19,6 +22,9 @@ fn main() {
 fn solution(day: u8, s: String) -> day::Day {
     match day {
         1 => day1::Day1::new(s),
+        2 => day2::Day2::new(s),
+        3 => day3::Day3::new(s),
+        4 => day4::Day4::new(s),
         12 => day12::Day12::new(s),
         15 => day15::Day15::new(s),
         _ => panic!("No solution branch for day {day}"),
