1508da2986d7ab78e8bb484e8e863fcc0dd5f7a0
Jochen Van de Voorde <jochen.van.de.voorde@persgroep.net>
2022-12-28 23:16:03 +0100

    Rename integration tests

--- 2022/rust/aoc2022/tests/day5-int-test.rs
+++ 2022/rust/aoc2022/tests/day5-int-test.rs
@@ -3,11 +3,11 @@ use aoc2022::{day::Solution, day5::Day5};
 mod common;
 
 #[test]
-fn part1() {
+fn day5_part1() {
     assert_eq!(Day5::part1(&common::get_test_input(5)), "CMZ");
 }
 
 #[test]
-fn part2() {
+fn day5_part2() {
     assert_eq!(Day5::part2(&common::get_test_input(5)), "MCD");
 }
