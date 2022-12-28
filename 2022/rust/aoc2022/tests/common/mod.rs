9e4e9a3f6c4660d2eb749b6fe4d96eb560ed302d
Jochen Van de Voorde <jochen.van.de.voorde@persgroep.net>
2022-12-28 23:15:39 +0100

    Handle failure reading test input

--- 2022/rust/aoc2022/tests/common/mod.rs
+++ 2022/rust/aoc2022/tests/common/mod.rs
@@ -1,6 +1,9 @@
 use std::fs;
 
 pub fn get_test_input(day: u8) -> String {
-    let puzzle_input_filename = format!("./resources/day{day}-test.txt");
-    fs::read_to_string(&puzzle_input_filename).ok().unwrap()
+    let puzzle_input_filename = format!("./tests/resources/day{day}-test.txt");
+    match fs::read_to_string(&puzzle_input_filename) {
+        Ok(s) => s,
+        Err(e) => panic!("Failed to get puzzle input for {puzzle_input_filename}, msg: {e}"),
+    }
 }
